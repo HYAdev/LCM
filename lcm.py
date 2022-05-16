1a89538d234191ec2c89aa2edd410dc9e39cf22f
HYENA <70421236+HYAdev@users.noreply.github.com>
2022-05-15 20:02:32 -0400

    Add files via upload

---
+++ lcm.py
@@ -0,0 +1,34 @@
+#find the least number that is evenly divisible from 1 through 20 (troll)
+#answer is 232792560
+
+x = 1
+
+while(True):
+    x += 1
+    if(x % 1 == 0):
+        if(x % 2 == 0):
+            if(x % 3 == 0):
+                if(x % 4 == 0):
+                    if(x % 5 == 0):
+                        if(x % 6 == 0):
+                            if(x % 7 == 0):
+                                if(x % 8 == 0):
+                                    if(x % 9 == 0):
+                                        if(x % 10 == 0):
+                                            if(x % 11 == 0):
+                                                if(x % 12 == 0):
+                                                    if(x % 13 == 0):
+                                                        if(x % 14 == 0):
+                                                            if(x % 15 == 0):
+                                                                if(x % 16 == 0):
+                                                                    if(x % 17 == 0):
+                                                                        if(x % 18 == 0):
+                                                                            if(x % 19 == 0):
+                                                                                if(x % 20 == 0):
+                                                                                    print(x)
+                                                                                    break
+
+
+
+                
+        
